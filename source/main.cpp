6e9b19000a654401ef90f3338ce12d490964addb
KL-Huang <craint1001@gmail.com>
2021-05-12 13:51:28 +0800

    commit

--- source/main.cpp
+++ source/main.cpp
@@ -1,51 +1,20 @@
-/* Sockets Example
- * Copyright (c) 2016-2020 ARM Limited
- *
- * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
- *
- *     http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
- */
-
 #include "mbed.h"
 #include "wifi_helper.h"
 #include "mbed-trace/mbed_trace.h"
 
-#if MBED_CONF_APP_USE_TLS_SOCKET
-#include "root_ca_cert.h"
-
-#ifndef DEVICE_TRNG
-#error "mbed-os-example-tls-socket requires a device which supports TRNG"
-#endif
-#endif // MBED_CONF_APP_USE_TLS_SOCKET
-
 class SocketDemo {
     static constexpr size_t MAX_NUMBER_OF_ACCESS_POINTS = 10;
     static constexpr size_t MAX_MESSAGE_RECEIVED_LENGTH = 100;
-
-#if MBED_CONF_APP_USE_TLS_SOCKET
-    static constexpr size_t REMOTE_PORT = 443; // tls port
-#else
+    static constexpr char*  HOST_IP="127.0.0.1";
     static constexpr size_t REMOTE_PORT = 80; // standard HTTP port
-#endif // MBED_CONF_APP_USE_TLS_SOCKET
 
 public:
-    SocketDemo() : _net(NetworkInterface::get_default_instance())
-    {
-    }
+    SocketDemo() : _net(NetworkInterface::get_default_instance()) {}
 
     ~SocketDemo()
     {
-        if (_net) {
+        if (_net)
             _net->disconnect();
-        }
     }
 
     void run()
@@ -54,18 +23,6 @@ public:
             printf("Error! No network interface found.\r\n");
             return;
         }
-
-        /* if we're using a wifi interface run a quick scan */
-        if (_net->wifiInterface()) {
-            /* the scan is not required to connect and only serves to show visible access points */
-            wifi_scan();
-
-            /* in this example we use credentials configured at compile time which are used by
-             * NetworkInterface::connect() but it's possible to do this at runtime by using the
-             * WiFiInterface::connect() which takes these parameters as arguments */
-        }
-
-        /* connect will perform the action appropriate to the interface type to connect to the network */
 
         printf("Connecting to the network...\r\n");
 
@@ -77,34 +34,16 @@ public:
 
         print_network_info();
 
-        /* opening the socket only allocates resources */
         result = _socket.open(_net);
         if (result != 0) {
             printf("Error! _socket.open() returned: %d\r\n", result);
             return;
         }
 
-#if MBED_CONF_APP_USE_TLS_SOCKET
-        result = _socket.set_root_ca_cert(root_ca_cert);
-        if (result != NSAPI_ERROR_OK) {
-            printf("Error: _socket.set_root_ca_cert() returned %d\n", result);
-            return;
-        }
-        _socket.set_hostname(MBED_CONF_APP_HOSTNAME);
-#endif // MBED_CONF_APP_USE_TLS_SOCKET
+        SocketAddress address(HOST_IP, REMOTE_PORT); //Set IP and port directly
 
-        /* now we have to find where to connect */
-
-        SocketAddress address;
-
-        if (!resolve_hostname(address)) {
-            return;
-        }
-
-        address.set_port(REMOTE_PORT);
-
-        /* we are connected to the network but since we're using a connection oriented
-         * protocol we still need to open a connection on the socket */
+        //if (!resolve_hostname(address)) return;
+        //address.set_port(REMOTE_PORT);
 
         printf("Opening connection to remote port %d\r\n", REMOTE_PORT);
 
@@ -114,15 +53,8 @@ public:
             return;
         }
 
-        /* exchange an HTTP request and response */
-
-        if (!send_http_request()) {
+        if (!send_http_request() || !receive_http_response())
             return;
-        }
-
-        if (!receive_http_response()) {
-            return;
-        }
 
         printf("Demo concluded successfully \r\n");
     }
@@ -132,7 +64,6 @@ private:
     {
         const char hostname[] = MBED_CONF_APP_HOSTNAME;
 
-        /* get the host address */
         printf("\nResolve hostname %s\r\n", hostname);
         nsapi_size_or_error_t result = _net->gethostbyname(hostname, &address);
         if (result != 0) {
@@ -148,10 +79,7 @@ private:
     bool send_http_request()
     {
         /* loop until whole request sent */
-        const char buffer[] = "GET / HTTP/1.1\r\n"
-                              "Host: ifconfig.io\r\n"
-                              "Connection: close\r\n"
-                              "\r\n";
+        const char buffer[] = "GET / HTTP/1.1\r\n" "Host: 127.0.0.1\r\n" "Connection: close\r\n" "\r\n";
 
         nsapi_size_t bytes_to_send = strlen(buffer);
         nsapi_size_or_error_t bytes_sent = 0;
@@ -163,9 +91,8 @@ private:
             if (bytes_sent < 0) {
                 printf("Error! _socket.send() returned: %d\r\n", bytes_sent);
                 return false;
-            } else {
+            } else
                 printf("sent %d bytes\r\n", bytes_sent);
-            }
 
             bytes_to_send -= bytes_sent;
         }
@@ -184,7 +111,7 @@ private:
         /* loop until there is nothing received or we've ran out of buffer space */
         nsapi_size_or_error_t result = remaining_bytes;
         while (result > 0 && remaining_bytes > 0) {
-            result = _socket.recv(buffer + received_bytes, remaining_bytes);
+            nsapi_size_or_error_t result = _socket.recv(buffer + received_bytes, remaining_bytes);
             if (result < 0) {
                 printf("Error! _socket.recv() returned: %d\r\n", result);
                 return false;
@@ -194,42 +121,13 @@ private:
             remaining_bytes -= result;
         }
 
-        /* the message is likely larger but we only want the HTTP response code */
-
         printf("received %d bytes:\r\n%.*s\r\n\r\n", received_bytes, strstr(buffer, "\n") - buffer, buffer);
 
         return true;
     }
 
-    void wifi_scan()
-    {
-        WiFiInterface *wifi = _net->wifiInterface();
-
-        WiFiAccessPoint ap[MAX_NUMBER_OF_ACCESS_POINTS];
-
-        /* scan call returns number of access points found */
-        int result = wifi->scan(ap, MAX_NUMBER_OF_ACCESS_POINTS);
-
-        if (result <= 0) {
-            printf("WiFiInterface::scan() failed with return value: %d\r\n", result);
-            return;
-        }
-
-        printf("%d networks available:\r\n", result);
-
-        for (int i = 0; i < result; i++) {
-            printf("Network: %s secured: %s BSSID: %hhX:%hhX:%hhX:%hhx:%hhx:%hhx RSSI: %hhd Ch: %hhd\r\n",
-                   ap[i].get_ssid(), get_security_string(ap[i].get_security()),
-                   ap[i].get_bssid()[0], ap[i].get_bssid()[1], ap[i].get_bssid()[2],
-                   ap[i].get_bssid()[3], ap[i].get_bssid()[4], ap[i].get_bssid()[5],
-                   ap[i].get_rssi(), ap[i].get_channel());
-        }
-        printf("\r\n");
-    }
-
     void print_network_info()
     {
-        /* print the network info */
         SocketAddress a;
         _net->get_ip_address(&a);
         printf("IP address: %s\r\n", a.get_ip_address() ? a.get_ip_address() : "None");
@@ -241,12 +139,7 @@ private:
 
 private:
     NetworkInterface *_net;
-
-#if MBED_CONF_APP_USE_TLS_SOCKET
-    TLSSocket _socket;
-#else
     TCPSocket _socket;
-#endif // MBED_CONF_APP_USE_TLS_SOCKET
 };
 
 int main() {
